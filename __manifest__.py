a1d46c5ae77ec32a0ef059fbd472cb671756ba3e
Ayesha Chowdhury <147982226+ai0585413@users.noreply.github.com>
2025-09-14 18:02:14 +0600

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'bKash Payment Gateway',
+    'version': '1.0',
+    'category': 'Accounting/Payment Acquirers',
+    'summary': 'Integration of bKash Payment Gateway',
+    'description': """Pay with bKash directly from Odoo eCommerce and Invoicing.""",
+    'author': 'Ayesha Chowdhury',
+    'depends': ['base','website','payment'],
+    'data': [
+        'views/bkash_redirect_form.xml',
+        'views/payment_status_template.xml',
+        'data/payment_methods.xml', 
+        'data/payment_provider_data.xml',
+        'views/payment_bkash_views.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
